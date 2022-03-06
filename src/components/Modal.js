a697e2a7cc925ce3870c97eb0a196e3ad51fc9c4
margaritasing <libiamfreitesm@gmail.com>
2022-03-06 19:43:50 -0300

    agrego estilos css

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,21 @@ import React from 'react'
 const Modal = () => {
   return (
     <>
-    
+    <div className="overlay"> 
+        <div className="overlay-inner">
+            <button className="close"> </button>
+            <div className="inner-box">
+                <img  />
+                <div className="info">
+                    <h1>sakhdjsahhjksfhd</h1>   
+                    <h3>kjahsdjhsjhdajhsajhdsdkjksaj</h3>
+                    <h4> lkakdlasjdljasjd <span>6565656</span> </h4> <br/>
+                    <a href=''> <button>More</button></a>           
+                </div>            
+            </div> 
+            <h4 className="description">klhsaflihaidhfidhsiofhiodshiohiodshcdkncjnxjcnudvjdasbv</h4>       
+        </div>   
+    </div>    
     </>
   )
 }
